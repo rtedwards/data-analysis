bad71a58d98dd684f3471016cafd3c9d948ba93c
Robert Edwards (student) <2416963E@student.gla.ac.uk>
2019-01-27 20:27:45 +0000

    Finished quiz1.  Got half point off...

---
+++ Quiz1.R
@@ -0,0 +1,106 @@
+## Data Analysis
+## Quiz 1
+
+# Question 1
+# What R function can be used to check the dimensions of a data set?
+glimpse()
+
+# Question 2
+# What is the meaning of the ! symbol in R?
+not
+
+# Question 3
+# Which definition best describes the arrange() function in R?
+Sort the rows based on one or more variables
+
+# Question 4
+# Which statement best describes the output of this code?
+A list of airlines and their number of seats available out of NYC, in ascending order.
+
+# Question 5
+flights %>%
+  group_by(month, origin) %>%
+  summarize(count = n())
+
+# Question 6
+# In a scatterplot, which variable is typically plotted on the y-axis?
+Outcome variable
+
+# Question 7
+# Which plotting technique is used to represent the five-number summary visually?
+Boxplot
+
+# Question 8
+# What does the orange highlighted part of the boxplot refer to?
+Median
+
+# Question 9
+# How is the Interquartile Range (IQR) caluclated?
+3rd quartile - 1st quartile
+
+# Question 10
+# Let a simple linear regression model be written as follows:
+# yi=??+??xi+??i,    ??i???N(0,??2),    i=1,.,n.
+# The explanatory variable is denoted by what?
+x_i 
+
+# Question 11
+# Which part of a regression model defines its mean?
+Deterministic part
+
+# Question 12
+# From the following scatterplot, describe the relationship between the continuous variables x and y:
+Moderate, positive linear relationship
+
+# Question 13
+# What is the verbal interpretation of a correlation coefficient of -0.91?
+Very strong negative correlation
+
+# Question 14
+# What is the minimum value (to 2 decimal places) for Petal Length?
+p0 = 1
+
+# Question 15
+# How would you describe the distribution of the continuous variable x from the histogram below?
+Left-skewed
+
+# Question 16
+# Given the observed value y=18.41 and the fitted value y^=19.00, compute (to 2 decimal places) the residual error ??^.
+18.41 - 19.00
+
+# Question 17
+# Are the residuals normally distributed with mean zero?
+Scatterplot of the residuals against an explanatory variable
+
+# Question 18
+ FALSE
+ 
+# Question 19
+# Does the assumption of constant variance of the residuals across all values of the explanatory variable hold?
+FALSE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
