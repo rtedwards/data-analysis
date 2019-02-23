d969f7cc7b68388791994d00f3a72a455629dabb
Robert <rtedwards.eng@gmail.com>
2019-02-23 12:37:19 +0000

    new directory for project and finished week6 lab

---
+++ project/interesting-dataset-eda.R
@@ -0,0 +1,3 @@
+fed_rd <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/fed_r_d_spending.csv")
+energy_spend <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/energy_spending.csv")
+climate_spend <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/climate_spending.csv")
